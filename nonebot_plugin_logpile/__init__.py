27cb14451f7a6d54633f5ed051db0f8efa2a0a7a
IllTamer <78360471+IllTamer@users.noreply.github.com>
2024-04-10 19:21:13 +0800

    ✨ 修复了无法加载配置的问题，重构日志文件注册逻辑 (#2)

--- nonebot_plugin_logpile/__init__.py
+++ nonebot_plugin_logpile/__init__.py
@@ -1,11 +1,19 @@
 from datetime import datetime
+from nonebot import get_plugin_config
+from nonebot.plugin import PluginMetadata
 from typing import Any, Dict, List, Tuple, Union, get_args
 
-from nonebot import get_driver, logger
+from nonebot import logger
 
 from .config import Config, LevelName
 
-pc = Config.parse_obj(get_driver().config)
+__plugin_meta__ = PluginMetadata(
+    name="nonebot-plugin-logpile",
+    description="将 nonebot 的日志记录保存到本地文件",
+    usage="",
+    config=Config,
+)
+pc = get_plugin_config(Config)
 
 LOG_CONFIG = {
     "rotation": "00:00",
@@ -20,8 +28,9 @@ def file_handler(
 ) -> List[Dict[str, Any]]:
     if not isinstance(levels, tuple):
         level_names = get_args(LevelName)
-        minimum = level_names.index(levels)
-        levels = level_names[minimum:]
+        # check if level exists
+        level_index = level_names.index(levels)
+        levels = tuple([level_names[level_index]])
     return [
         {
             "sink": pc.logpile_path
