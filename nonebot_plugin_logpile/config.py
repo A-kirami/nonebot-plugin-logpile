0c3d757e3fd78d04bbcfea3e7a0b86d63a79b56a
student_2333 <lgc2333@126.com>
2024-10-30 14:14:14 +0800

    refactor: pydantic 2 deprecated things

--- nonebot_plugin_logpile/config.py
+++ nonebot_plugin_logpile/config.py
@@ -1,18 +1,19 @@
 from pathlib import Path
 from typing import Literal, Tuple, Union
 
-from pydantic import BaseModel, Extra, validator
+from cookit.pyd import field_validator
+from pydantic import BaseModel
 
 LevelName = Literal["TRACE", "DEBUG", "INFO", "SUCCESS", "WARNING", "ERROR", "CRITICAL"]
 
 
-class Config(BaseModel, extra=Extra.ignore):
+class Config(BaseModel):
     logpile_path: Path = Path.cwd() / "logs"
     logpile_level: Union[LevelName, Tuple[LevelName]] = "ERROR"
     logpile_retention: int = 14
 
-    @validator("logpile_path")
-    def check_path(cls, v: Path) -> Path:
+    @field_validator("logpile_path")
+    def check_path(cls, v: Path) -> Path:  # noqa: N805
         if v.exists() and not v.is_dir():
             raise ValueError("必须是有效的文件目录")
         v.mkdir(parents=True, exist_ok=True)
